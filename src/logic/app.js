1ac47cda61d89448ce05703b596f8403e31b09e9
nikunokuni <76274183+nikunokuni@users.noreply.github.com>
2026-05-29 00:14:28 +0900

    Update app.js

--- src/logic/app.js
+++ src/logic/app.js
@@ -617,7 +617,7 @@ const App = {
   },
 
   sendFeedback() {
-    const FORM_URL = 'https://forms.gle/XEVhBG2636FCohLw9';
+    const FORM_URL = 'https://x.gd/Jp9px';
     window.open(FORM_URL, '_blank', 'noopener,noreferrer');
   },
 
